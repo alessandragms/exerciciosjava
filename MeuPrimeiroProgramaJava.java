f6ce1d0706c51c3179f95e485b13bc2538aef9a9
Alessandra Gomes <alessandragomes.gomes@gmail.com>
2025-06-16 20:55:13 -0300

    Create MeuPrimeiroProgramaJava.java

---
+++ MeuPrimeiroProgramaJava.java
@@ -0,0 +1,19 @@
+//nome da classe
+class MeuPrimeiroProgramaJava
+{
+	public static void main (String entrda[])
+	//modulo principal com a entrada pela linha de comando
+	{
+		int inteiro = 23;
+		char caracter = 'F';
+		double real = 1.59;
+		String frase = "Alessandra";
+		boolean VF = true; 
+		
+		if (VF == true)
+		{
+			System.out.println("Eu sou a " + frase + "tenho " + inteiro + " anos e tenho" + real + "m de altura");
+		}
+	System.exit(0);
+	}
+}
